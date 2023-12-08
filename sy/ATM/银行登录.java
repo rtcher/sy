3d38464178e1e707597401a76212f94554477284
rtcher <2443286912@qq.com>
2023-12-08 14:46:44 +0800

    1.0.0

--- sy/ATM/银行登录.java
+++ sy/ATM/银行登录.java
@@ -2,9 +2,10 @@ import javax.swing.*;
 
 public class 银行登录 {
     private JButton 登录Button;
-    private JPanel panel1;
+    private JPanel 登录界面;
     private JList 标题;
-    private JTextField textField1;
+    private JTextField 请输入账号密码;
+    private JPasswordField 银行卡密码;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
