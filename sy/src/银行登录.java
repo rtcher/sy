3f1470915f4469bbfc392df2aea2a5c4807cc5c7
rtcher <2443286912@qq.com>
2023-12-08 14:04:28 +0800

    1.0.0

---
+++ sy/src/银行登录.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class 银行登录 {
+    private JButton 登录Button;
+    private JPanel panel1;
+    private JList 标题;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
