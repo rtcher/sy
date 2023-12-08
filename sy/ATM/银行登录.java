12148a80daf55d84f8411e40994d804cdcc650d9
rtcher <2443286912@qq.com>
2023-12-08 15:30:00 +0800

    1.0.0

--- sy/ATM/银行登录.java
+++ sy/ATM/银行登录.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class 银行登录 {
     private JButton 登录按钮;
@@ -6,6 +8,15 @@ public class 银行登录 {
     private JList 标题;
     private JTextField 请输入账号密码;
     private JPasswordField 银行卡密码;
+
+    public 银行登录() {
+        登录按钮.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
